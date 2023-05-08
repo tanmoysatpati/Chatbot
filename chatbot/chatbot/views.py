e92deb3d2f4805d6811ea2f7c9db6fb92faf75c5
tsatpati <tanmoyonl9app@gmail.com>
2023-05-08 15:59:57 +0530

    Chatbot commit

---
+++ chatbot/chatbot/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import openai, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_KEY", None)
+
+
+def chatbot(request):
+    chatbot_response = None
+    if api_key != None and request.method == 'POST':
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+
+        response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=256,
+        temperature=0.5
+        )
+        print(response)
+        chatbot_response=response["choices"][0]["text"]
+    return render(request, 'main.html', {"response" : chatbot_response})
